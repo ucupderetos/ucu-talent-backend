356682a6607143d48507399a168f632b76466961
Facundo Gallinal <facundo.gallinal@correo.ucu.edu.uy>
2026-07-17 21:45:53 -0300

    fix(VacancyApplicationServiceImpl): fix errors

--- src/main/java/ucu/retojulio2026/talent/area/AreaRepository.java
+++
@@ -1,8 +0,0 @@
-package ucu.retojulio2026.talent.area;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AreaRepository extends JpaRepository<Area, String> {
-}
--- src/main/java/ucu/retojulio2026/talent/universityregistry/UniversityRegistryRepository.java
+++
@@ -1,8 +0,0 @@
-package ucu.retojulio2026.talent.universityregistry;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UniversityRegistryRepository extends JpaRepository<UniversityRegistry, String> {
-}
