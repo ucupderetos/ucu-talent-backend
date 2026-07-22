bde6e0b96e2edc6684be3fb9c74d6567de7f59f0
AgustinG130 <AGUSTINANDRES.GARCIA@CORREO.UCU.EDU.UY>
2026-07-22 00:28:08 -0300

    fix: vacancies with applications can't be edited, only verified accounts can create vacancies and added PATCH endpoint

--- src/main/java/ucu/retojulio2026/talent/vacancy/dto/UpdateVacancyStatusAdminRequest.java
+++ src/main/java/ucu/retojulio2026/talent/vacancy/dto/UpdateVacancyStatusAdminRequest.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import ucu.retojulio2026.talent.vacancy.VacancyStatus;
 
-public record UpdateVacancyStatusRequest(
+public record UpdateVacancyStatusAdminRequest(
+        @Schema(description = "El comentario del administrador, en caso que sea necesario")
+        String adminComment,
         @Schema(description = "El estado del Puesto", example = "PUBLICADO")
         @NotNull(message = "El nuevo estado es obligatorio")
         VacancyStatus status
