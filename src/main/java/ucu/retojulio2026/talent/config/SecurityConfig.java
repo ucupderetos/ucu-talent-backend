fa3512fddcdc8cbf43d295e71022972c6830ecbe
AgustinG130 <AGUSTINANDRES.GARCIA@CORREO.UCU.EDU.UY>
2026-07-30 17:18:45 -0300

    fix: authenticated user can change profile image

--- src/main/java/ucu/retojulio2026/talent/config/SecurityConfig.java
+++ src/main/java/ucu/retojulio2026/talent/config/SecurityConfig.java
@@ -188,7 +188,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/vacancy-application").hasRole("ALUMNO")
                         .requestMatchers(HttpMethod.GET, "/vacancy-application/me").hasRole("ALUMNO")
                         .requestMatchers(HttpMethod.GET, "/vacancy-application/me/detailed").hasRole("ALUMNO")
-                        .requestMatchers(HttpMethod.PATCH, "/user/profile/image").hasRole("ALUMNO")
+                        .requestMatchers(HttpMethod.PATCH, "/user/profile/image").authenticated()
                         // Admin
                         .requestMatchers(HttpMethod.POST, "/audit/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/audit/**").hasRole("ADMIN")
